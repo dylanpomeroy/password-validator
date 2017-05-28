80aaa1a885550e15b29b851cc556f70bfb5413e4
Dylan Pomeroy <dylan.pomeroy@dal.ca>
2017-05-28 20:26:54 -0300

    finished stage 2

--- app/src/main/java/com/example/dylanpomeroy/passwordvalidator/Password.java
+++ app/src/main/java/com/example/dylanpomeroy/passwordvalidator/Password.java
@@ -4,6 +4,9 @@ package com.example.dylanpomeroy.passwordvalidator;
  * Created by dylanpomeroy on 5/28/17.
  */
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class Password {
     public String text;
 
@@ -12,11 +15,17 @@ public class Password {
     }
 
     public boolean validate(){
-        if (this.text == "password"
-                || this.text.length() < 8){
-            return false;
-        }
 
+        ArrayList<Pattern> patterns = new ArrayList<Pattern>();
+        patterns.add(Pattern.compile(".{8,}"));
+        patterns.add(Pattern.compile(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*"));
+        patterns.add(Pattern.compile(".*[A-Z].*"));
+        patterns.add(Pattern.compile(".*[a-z].*"));
+        patterns.add(Pattern.compile(".*[0-9].*"));
+
+        for (Pattern pattern : patterns)
+            if (!pattern.matcher(this.text).matches())
+                return false;
         return true;
     }
 }
--- app/src/test/java/com/example/dylanpomeroy/passwordvalidator/ExampleUnitTest.java
+++ app/src/test/java/com/example/dylanpomeroy/passwordvalidator/ExampleUnitTest.java
@@ -15,7 +15,22 @@ public class ExampleUnitTest {
         Password password = new Password("password");
         assertFalse(password.validate());
 
-        password.text = "2short";
+        password.text = "2$h0rT";
         assertFalse(password.validate());
+
+        password.text = "noupperca$3";
+        assertFalse(password.validate());
+
+        password.text = "NOLOWERCA$3";
+        assertFalse(password.validate());
+
+        password.text = "NoSymbols";
+        assertFalse(password.validate());
+
+        password.text = "ValidPas$word1";
+        assertTrue(password.validate());
+
+        password.text = "V4l1dP4$$w0rd2";
+        assertTrue(password.validate());
     }
 }
