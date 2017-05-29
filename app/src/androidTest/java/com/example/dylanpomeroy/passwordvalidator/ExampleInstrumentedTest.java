2c5ddf9d138fa05fa657ddf963f40237b80221e4
Dylan Pomeroy <dylan.pomeroy@dal.ca>
2017-05-28 21:14:16 -0300

    completed stage 3

--- app/src/androidTest/java/com/example/dylanpomeroy/passwordvalidator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/dylanpomeroy/passwordvalidator/ExampleInstrumentedTest.java
@@ -1,12 +1,24 @@
 package com.example.dylanpomeroy.passwordvalidator;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +28,33 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class) { };
+
+    @Test
+    public void tryPassword1(){
+        onView(withId(R.id.txtPassEntry))
+                .perform(typeText("#herpAd3rp"), closeSoftKeyboard());
+
+        onView(withId(R.id.btnValidate))
+                .perform(click());
+
+        onView(withId(R.id.lblPassStrength))
+                .check(matches(withText(R.string.strongPassMsg)));
+    }
+
+    @Test
+    public void tryPassword2(){
+        onView(withId(R.id.txtPassEntry))
+                .perform(typeText("weakazpass"), closeSoftKeyboard());
+
+        onView(withId(R.id.btnValidate))
+                .perform(click());
+
+        onView(withId(R.id.lblPassStrength))
+                .check(matches(withText(R.string.weakPassMsg)));
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
--- app/src/main/java/com/example/dylanpomeroy/passwordvalidator/MainActivity.java
+++ app/src/main/java/com/example/dylanpomeroy/passwordvalidator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dylanpomeroy.passwordvalidator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkPassword(View view){
+        Password password = new Password(
+                ((EditText)findViewById(R.id.txtPassEntry))
+                        .getText().toString());
+
+        if (password.validate()){
+            ((TextView)findViewById(R.id.lblPassStrength))
+                    .setText(R.string.strongPassMsg);
+        }
+        else {
+            ((TextView)findViewById(R.id.lblPassStrength))
+                    .setText(R.string.weakPassMsg);
+        }
     }
 }
